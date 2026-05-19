2a062d8a536b77b6e1593a5af113bb891322b419
xpzwzwz <2294549920@qq.com>
2026-05-19 19:04:59 +0800

    feat: add family dish browsing flow

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,28 +1,31 @@
 import { getCategoryList } from '../../services/good/fetchCategoryList';
+
 Page({
   data: {
     list: [],
-  },
-  async init() {
-    try {
-      const result = await getCategoryList();
-      this.setData({
-        list: result,
-      });
-    } catch (error) {
-      console.error('err:', error);
-    }
   },
 
   onShow() {
     this.getTabBar().init();
   },
-  onChange() {
-    wx.navigateTo({
-      url: '/pages/goods/list/index',
-    });
-  },
+
   onLoad() {
-    this.init(true);
+    this.init();
+  },
+
+  async init() {
+    try {
+      const list = await getCategoryList();
+      this.setData({ list });
+    } catch (error) {
+      console.error('load dish categories failed:', error);
+    }
+  },
+
+  openCategory(event) {
+    const { id, name } = event.currentTarget.dataset;
+    wx.navigateTo({
+      url: `/pages/goods/list/index?categoryId=${id}&categoryName=${encodeURIComponent(name)}`,
+    });
   },
 });
